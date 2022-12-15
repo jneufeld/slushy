edd11617503a292ea5d4d8010ffbb519dc4e2115
Jonathan Neufeld <jneufeld@alumni.ubc.ca>
2022-12-15 15:23:21 -0800

    Add poor attempt at day 15

--- src/day15/mod.rs
+++ src/day15/mod.rs
@@ -6,6 +6,16 @@ use regex::Regex;
 pub fn solve() {
     let input = SAMPLE;
     let report = Report::from(input);
+
+    let magic_y = 10;
+
+    let count = report
+        .scanned
+        .iter()
+        .filter(|position| position.y == magic_y)
+        .count();
+
+    println!("count at y={}: {}", magic_y, count);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -17,6 +27,47 @@ struct Position {
 impl Position {
     fn new(x: isize, y: isize) -> Self {
         Position { x, y }
+    }
+
+    fn get_scanned(&self, beacon: Position) -> HashSet<Position> {
+        let mut scanned = HashSet::new();
+
+        let distance = self.get_manhattan_distance(beacon);
+
+        println!(
+            "distance between {:?} and {:?} is {}",
+            self, beacon, distance
+        );
+
+        for x in 0..distance + 1 {
+            let left_x = self.x - x as isize;
+            let right_x = self.x + x as isize;
+
+            for y in 0..distance + 1 - x {
+                let up_y = self.y + y as isize;
+                let down_y = self.y - y as isize;
+
+                //println!(
+                //    "\tadding ({}, {}), ({}, {}), ({}, {}), and ({}, {})",
+                //    left_x,
+                //    up_y,
+                //    left_x,
+                //    down_y,
+                //    right_x,
+                //    up_y,
+                //    right_x,
+                //    down_y
+                //);
+
+                scanned.insert(Position::new(left_x, up_y));
+                scanned.insert(Position::new(left_x, down_y));
+
+                scanned.insert(Position::new(right_x, up_y));
+                scanned.insert(Position::new(right_x, down_y));
+            }
+        }
+
+        scanned
     }
 
     fn get_manhattan_distance(&self, other: Position) -> usize {
@@ -51,7 +102,7 @@ impl Square {
 struct Report {
     sensors: HashSet<Position>,
     beacons: HashSet<Position>,
-    squares: Vec<Square>,
+    scanned: HashSet<Position>,
 }
 
 lazy_static! {
@@ -65,41 +116,41 @@ impl From<&str> for Report {
     fn from(input: &str) -> Self {
         let mut sensors = HashSet::new();
         let mut beacons = HashSet::new();
-        let mut squares: Vec<Square> = Vec::new();
+        let mut scanned = HashSet::new();
 
         for line in input.split('\n') {
-            let captures = LINE_REGEX.captures(line).unwrap();
-
-            let sensor_x = captures.get(1).unwrap().as_str().parse().unwrap();
-            let sensor_y = captures.get(2).unwrap().as_str().parse().unwrap();
-
-            let sensor = Position::new(sensor_x, sensor_y);
+            let (sensor, beacon) = parse_positions(line);
 
             sensors.insert(sensor);
-
-            let beacon_x = captures.get(3).unwrap().as_str().parse().unwrap();
-            let beacon_y = captures.get(4).unwrap().as_str().parse().unwrap();
-
-            let beacon = Position::new(beacon_x, beacon_y);
-
             beacons.insert(beacon);
 
-            let square = Square::new(sensor, beacon);
+            scanned.extend(sensor.get_scanned(beacon));
 
-            squares.push(square);
-
-            print!(
-                "sensor: {:?}\nbeacon: {:?}\nsquare: {:?}\n\n",
-                sensor, beacon, square
-            );
+            print!("sensor: {:?}\nbeacon: {:?}\n\n", sensor, beacon);
         }
 
         Report {
             sensors,
             beacons,
-            squares,
+            scanned,
         }
     }
+}
+
+fn parse_positions(line: &str) -> (Position, Position) {
+    let captures = LINE_REGEX.captures(line).unwrap();
+
+    let sensor_x = captures.get(1).unwrap().as_str().parse().unwrap();
+    let sensor_y = captures.get(2).unwrap().as_str().parse().unwrap();
+
+    let sensor = Position::new(sensor_x, sensor_y);
+
+    let beacon_x = captures.get(3).unwrap().as_str().parse().unwrap();
+    let beacon_y = captures.get(4).unwrap().as_str().parse().unwrap();
+
+    let beacon = Position::new(beacon_x, beacon_y);
+
+    (sensor, beacon)
 }
 
 const SAMPLE: &str = r"Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -116,3 +167,30 @@ Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+
+const PUZZLE: &str = r"Sensor at x=3772068, y=2853720: closest beacon is at x=4068389, y=2345925
+Sensor at x=78607, y=2544104: closest beacon is at x=-152196, y=4183739
+Sensor at x=3239531, y=3939220: closest beacon is at x=3568548, y=4206192
+Sensor at x=339124, y=989831: closest beacon is at x=570292, y=1048239
+Sensor at x=3957534, y=2132743: closest beacon is at x=3897332, y=2000000
+Sensor at x=1882965, y=3426126: closest beacon is at x=2580484, y=3654136
+Sensor at x=1159443, y=3861139: closest beacon is at x=2580484, y=3654136
+Sensor at x=2433461, y=287013: closest beacon is at x=2088099, y=-190228
+Sensor at x=3004122, y=3483833: closest beacon is at x=2580484, y=3654136
+Sensor at x=3571821, y=799602: closest beacon is at x=3897332, y=2000000
+Sensor at x=2376562, y=1539540: closest beacon is at x=2700909, y=2519581
+Sensor at x=785113, y=1273008: closest beacon is at x=570292, y=1048239
+Sensor at x=1990787, y=38164: closest beacon is at x=2088099, y=-190228
+Sensor at x=3993778, y=3482849: closest beacon is at x=4247709, y=3561264
+Sensor at x=3821391, y=3986080: closest beacon is at x=3568548, y=4206192
+Sensor at x=2703294, y=3999015: closest beacon is at x=2580484, y=3654136
+Sensor at x=1448314, y=2210094: closest beacon is at x=2700909, y=2519581
+Sensor at x=3351224, y=2364892: closest beacon is at x=4068389, y=2345925
+Sensor at x=196419, y=3491556: closest beacon is at x=-152196, y=4183739
+Sensor at x=175004, y=138614: closest beacon is at x=570292, y=1048239
+Sensor at x=1618460, y=806488: closest beacon is at x=570292, y=1048239
+Sensor at x=3974730, y=1940193: closest beacon is at x=3897332, y=2000000
+Sensor at x=2995314, y=2961775: closest beacon is at x=2700909, y=2519581
+Sensor at x=105378, y=1513086: closest beacon is at x=570292, y=1048239
+Sensor at x=3576958, y=3665667: closest beacon is at x=3568548, y=4206192
+Sensor at x=2712265, y=2155055: closest beacon is at x=2700909, y=2519581";
